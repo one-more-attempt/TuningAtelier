854114710ec1380ab0610f953740c52c573e12f0
one-more-attempt <78234138+one-more-attempt@users.noreply.github.com>
2022-02-21 16:58:37 +0300

    Add files via upload

--- HW1/array.js
+++ HW1/array.js
@@ -3,14 +3,18 @@
 //  (сделайте из нее '654321') не используя цикл
 const inputString = "123456";
 console.log(inputString);
-
 const ReverseString = (input) => input.split("").reverse().join("");
 console.log(ReverseString(inputString));
+
+
 
 //Проверьте, что строка начинается на http:// и заканчивается на .html.
 const inputURL = "http://onliner.by/index.html";
 const checkURL = (input) => input.startsWith("http://") && input.endsWith(".html")
 console.log(checkURL(inputURL));
+
+
+
 
 //Сделайте функцию, которая принимает параметром число от 1 до 7,
 // а возвращает день недели на русском языке.
@@ -32,7 +36,8 @@ function RusWeekDayFromNumber(inputNumber) {
 console.log(RusWeekDayFromNumber(6));
 
 
-//CODEWARS
+
+//CODEWARS 1 (string/array methods)
 // It should look like this: Sam Harris => S.H; patrick feeney => P.F
 function abbreviateFullName (fullName){
 const splittedName = fullName.split(' ');
@@ -40,7 +45,9 @@ return(`${splittedName[0].slice(0,1)}.${splittedName[1].slice(0,1)}`.toUpperCase
 }
 console.log(abbreviateFullName('Sam Harris'));
 
-//CODEWARS
+
+
+//CODEWARS 2 (array methods)
 // Return an array, where the first element is the count of positives numbers and the second
 //  element is sum of negative numbers
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15],
@@ -57,9 +64,7 @@ function countSum(input) {
 
   input.forEach((item) => {
     if (item > 0) ++positiveCount;
-    else {
-      negativeSumm += item;
-    }
+    else  negativeSumm += item
   });
   outputArray.push(positiveCount, negativeSumm);
   console.log (outputArray)
@@ -67,3 +72,58 @@ function countSum(input) {
 }
 countSum(testArray);
 
+
+
+//codewars 3 (reduce)
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+function arrayReduceMultiply (inputArray){
+   return  inputArray.reduce ((prev, curr)=> {return (prev*curr)},1)
+ }
+console.log (arrayReduceMultiply([1,2,3,4]))
+
+
+
+//codewars 4 (array methods)
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+console.clear();
+function DNAComplemets (inputDNA){
+  const complements = {
+    A: "T",
+    T: 'A',
+    C : "G",
+    G: 'C',
+  };
+  return inputDNA.toUpperCase().
+  split('').map ((item, index, array)=> complements[item]).join('')
+}
+console.log (DNAComplemets ('ATTGC'))
+
+//using replace
+function DNAComplemetsUsingReplace (inputDNA){
+  return inputDNA.toLowerCase().replace(/a/g, "T").replace(/t/g, "A")
+    .replace(/c/g, "G").replace(/g/g, "C")
+}
+console.log (DNAComplemetsUsingReplace ('ATTGC'))
+
+//codewars 5
+//усреднить значения в каждом элементе 
+
+let matrix =[
+  [ [123, 231, 12], [56, 43, 124] ],
+  [ [78, 152, 76], [64, 132, 200] ]
+ ]
+
+
+function RGB (matrix){
+  return matrix.map (row => row.map(itemInRow=>
+      Array (3).fill(Math.round (itemInRow.reduce((a,b)=>a+b,0)/3))   ))
+    }
+
+console.log (RGB(matrix))
+
+// let tmpArray = matrix.map (row => row.map(itemInRow=>
+//   Array (3).fill(Math.round (itemInRow.reduce((a,b)=>a+b,0)/3))   ))
+// console.log (tmpArray)
